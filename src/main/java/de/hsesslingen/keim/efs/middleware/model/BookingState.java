b5702d5638e24362e61b3ddc640201817703637c
Ben Oesch <ben.oesch@hs-esslingen.de>
2021-07-08 15:58:57 +0200

    Added BookingState PAUSED.

--- src/main/java/de/hsesslingen/keim/efs/middleware/model/BookingState.java
+++ src/main/java/de/hsesslingen/keim/efs/middleware/model/BookingState.java
@@ -1,30 +1,30 @@
 /*
  * MIT License
- * 
+ *
  * Copyright (c) 2020 Hochschule Esslingen
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE. 
+ * SOFTWARE.
  */
 package de.hsesslingen.keim.efs.middleware.model;
 
 /**
- * The life,cycle state of the booking (from NEW to FINISHED)
+ * The life-cycle state of a booking.
  *
  * @author boesch
  */
@@ -33,9 +33,10 @@ public enum BookingState {
     BOOKED, // Followed by: CANCELLED | STARTED | UPDATEREQUESTED
     CANCELLED, // Closed
     UPDATEREQUESTED, // Kept as before
-    STARTED, // Followed by: ABORTED | FINISHED | UPDATEREQUESTED
+    STARTED, // Followed by: PAUSED | ABORTED | FINISHED | UPDATEREQUESTED
     ABORTED, // Closed
-    FINISHED; // Closed
+    FINISHED, // Closed
+    PAUSED; // Followed by: STARTED | ABORTED | FINISHED | UPDATEREQUESTED
 
     public boolean canAdvanceTo(BookingState next) {
         if (next == null) {
@@ -48,7 +49,9 @@ public enum BookingState {
             case BOOKED:
                 return next == UPDATEREQUESTED || next == CANCELLED || next == STARTED;
             case STARTED:
-                return next == UPDATEREQUESTED || next == ABORTED || next == FINISHED;
+                return next == UPDATEREQUESTED || next == PAUSED || next == ABORTED || next == FINISHED;
+            case PAUSED:
+                return next == UPDATEREQUESTED || next == STARTED || next == ABORTED || next == FINISHED;
             case UPDATEREQUESTED:
                 return true;
             case CANCELLED:
